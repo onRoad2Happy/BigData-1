2efbe5a9d00eb72ebd595e1d2ba1df14adf361b2
xiangch <zengxc@foxmail.com>
2017-05-09 21:09:40 +0800

    修改run为yarn

--- res/conf/run.sh
+++ res/conf/run.sh
@@ -4,6 +4,14 @@ LIB_PATH=${LIB_PATH},$i
 done
 LIB_PATH=${LIB_PATH:0,2}
 #echo $LIB_PATH
-/usr/local/spark/bin/spark-submit --class com.wyd.BigData.App --files /usr/local/spark/conf/hive-site.xml --jars $LIB_PATH --master spark://Master:7077 ./BigData.jar >> stdout.log 2>&1 &
+/usr/local/spark/bin/spark-submit --class com.wyd.BigData.App \
+    --files /usr/local/spark/conf/hive-site.xml --jars $LIB_PATH \
+    --master yarn \
+    --deploy-mode cluster \
+    --driver-memory 512m \
+    --executor-memory 512m \
+    --executor-cores 1 \
+    --queue default \
+    ./BigData.jar > stdout.log 2>&1 &
 
 echo $! > bigdata.pid
