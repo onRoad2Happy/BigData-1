08686d396e8d5fab35fcfad65187cfac91bd4475
xiangch <zengxc@foxmail.com>
2017-05-16 10:38:46 +0800

    增加登陆Bean

---
+++ src/main/java/com/wyd/BigData/bean/LoginSumInfo.java
@@ -0,0 +1,60 @@
+package com.wyd.BigData.bean;
+public class LoginSumInfo {
+    private static final long serialVersionUID = 1L;
+    private int               id;
+    private int               serviceId;
+    private int               channelId;
+    private int               playerId;
+    private int               playerChannel;
+
+    public LoginSumInfo() {
+    }
+
+    public LoginSumInfo(int serviceId, int channelId, int playerChannel, int playerId) {
+        this.serviceId = serviceId;
+        this.channelId = channelId;
+        this.channelId = channelId;
+        this.playerId = playerId;
+        this.playerChannel = playerChannel;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getServiceId() {
+        return serviceId;
+    }
+
+    public void setServiceId(int serviceId) {
+        this.serviceId = serviceId;
+    }
+
+    public int getChannelId() {
+        return channelId;
+    }
+
+    public void setChannelId(int channelId) {
+        this.channelId = channelId;
+    }
+
+    public int getPlayerId() {
+        return playerId;
+    }
+
+    public void setPlayerId(int playerId) {
+        this.playerId = playerId;
+    }
+
+    public int getPlayerChannel() {
+        return playerChannel;
+    }
+
+    public void setPlayerChannel(int playerChannel) {
+        this.playerChannel = playerChannel;
+    }
+}
