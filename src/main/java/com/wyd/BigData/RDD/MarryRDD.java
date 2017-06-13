8ad3a08947a1a36b742c8343a44afd589a3e0b40
xiangch <zengxc@foxmail.com>
2017-06-13 15:44:56 +0800

    修改结婚统计为一次性统计四种等级的婚礼次数（之前是按等级统计）

--- src/main/java/com/wyd/BigData/RDD/MarryRDD.java
+++ src/main/java/com/wyd/BigData/RDD/MarryRDD.java
@@ -39,61 +39,66 @@ public class MarryRDD implements Serializable {
             }
             dao.updatePlayerMarryInfoBatch(playerInfoList);
         });
-        JavaPairRDD<String, Integer> counts = marryRDD.mapToPair(datas -> {
+        //KEY:serviceId_marryMark VAL:[1,marryType1,marryType2,marryType3,marryType4]
+        JavaPairRDD<String, Integer[]> counts = marryRDD.mapToPair(datas -> {
             BaseDao dao = BaseDao.getInstance();
             int groomId = Integer.parseInt(datas[2]);
             ServiceInfo serviceInfo = dao.getServiceInfo(groomId);
             int marryMark = Integer.parseInt(datas[4]);
             int marryType = Integer.parseInt(datas[5]);
+            int marryType1 = 0, marryType2 = 0, marryType3 = 0, marryType4 = 0;
+            switch (marryType) {
+            case 1:
+                marryType1 = 1;
+                break;
+            case 2:
+                marryType2 = 1;
+                break;
+            case 3:
+                marryType3 = 1;
+                break;
+            default:
+                marryType4 = 1;
+                break;
+            }
             int serviceId = serviceInfo == null ? -1 : serviceInfo.getServiceId();
-            String key = serviceId + "_" + marryMark + "_" + marryType;
-            return new Tuple2<>(key, 1);
-        }).reduceByKey((a, b) -> a + b);
+            String key = serviceId + "_" + marryMark;
+            return new Tuple2<>(key, new Integer[] { 1, marryType1, marryType2, marryType3, marryType4 });
+        }).reduceByKey((x, y) -> new Integer[] { x[0] + y[0], x[1] + y[1], x[2] + y[2], x[3] + y[3], x[4] + y[4] });
         counts.foreachPartition(it -> {
             BaseDao dao = BaseDao.getInstance();
-            List<MarryInfo> list1 = new ArrayList<>();
-            List<MarryInfo> list2 = new ArrayList<>();
-            List<MarryInfo> list3 = new ArrayList<>();
-            List<MarryInfo> list4 = new ArrayList<>();
+            List<MarryInfo> list = new ArrayList<>();
             while (it.hasNext()) {
-                Tuple2<String, Integer> t = it.next();
+                Tuple2<String, Integer[]> t = it.next();
                 String[] params = t._1().split("_");
                 int serviceId = Integer.parseInt(params[0]);
                 int marryMark = Integer.parseInt(params[1]);
-                int marryType = Integer.parseInt(params[2]);
-                int count = t._2();
+                int count = t._2()[0];
+                int marryType1 = t._2()[1];
+                int marryType2 = t._2()[2];
+                int marryType3 = t._2()[3];
+                int marryType4 = t._2()[4];
                 if (marryMark == 2) {
                     MarryInfo marryInfo = dao.getMarryInfo(serviceId);
                     if (marryInfo == null) {
                         marryInfo = new MarryInfo();
                         marryInfo.setServiceId(serviceId);
+                        marryInfo.setMarryNum(count);
+                        marryInfo.setLuxuriousNum(marryType1);
+                        marryInfo.setLuxuryNum(marryType2);
+                        marryInfo.setRomanticNum(marryType3);
+                        marryInfo.setGeneralNum(marryType4);
                         dao.saveMarryInfo(marryInfo);
                     }
-                    switch (marryType) {
-                    case 1:
-                        marryInfo.setLuxuriousNum(marryInfo.getLuxuriousNum() + count);
-                        list1.add(marryInfo);
-                        break;
-                    case 2:
-                        marryInfo.setLuxuryNum(marryInfo.getLuxuryNum() + count);
-                        list2.add(marryInfo);
-                        break;
-                    case 3:
-                        marryInfo.setRomanticNum(marryInfo.getRomanticNum() + count);
-                        list3.add(marryInfo);
-                        break;
-                    case 4:
-                        marryInfo.setGeneralNum(marryInfo.getGeneralNum() + count);
-                        list4.add(marryInfo);
-                        break;
-                    }
+                    marryInfo.setMarryNum(marryInfo.getMarryNum()+count);
+                    marryInfo.setLuxuriousNum(marryInfo.getLuxuriousNum() + marryType1);
+                    marryInfo.setLuxuryNum(marryInfo.getLuxuryNum() + marryType2);
+                    marryInfo.setRomanticNum(marryInfo.getRomanticNum() + marryType3);
+                    marryInfo.setGeneralNum(marryInfo.getGeneralNum() + marryType4);
+                    list.add(marryInfo);
                 }
             }
-            //不要更新未修改的值，未修改的值有可能在其它excutor里更改了，此时更新会把旧的值增覆盖上去
-            dao.updateMarryInfoBath(list1, 1);
-            dao.updateMarryInfoBath(list2, 2);
-            dao.updateMarryInfoBath(list3, 3);
-            dao.updateMarryInfoBath(list4, 4);
+            dao.updateMarryInfoBath(list, 0);
         });
     }
 }
